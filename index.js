363363c2d3e907e7207f2c2667d867c47af858ab
Alexa Anderson <alexa.anderson@outlook.com>
2016-06-28 21:33:03 -0500

    Change server to hapi

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io =  require('socket.io')(http);
+//// EXPRESS ////
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
-});
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io =  require('socket.io')(http);
+
+// app.get('/', function(request, response) {
+//   response.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function(socket) {
+//   console.log('a user connected');
+
+//   // on([emitted key], function([emitted value]){} )
+//   socket.on('chat-message', function(msg){
+//     io.emit('chat-message-from-server', msg);
+//   });
+//   socket.on('disconnect', function() {
+//     console.log('a user disconnected');
+//   });
+// });
+
+// http.listen(3000, function() {
+//   console.log("listening on 3000")
+// })
+
+
+//// HAPI ////
+
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+// socketio attaches itself to the listening server
+var io = require('socket.io')(server.listener);
+
+server.register(require('vision'), function(err) {
+  if (err) throw err;
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+      reply.view('index');
+    }
+  })
+
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: 'templates'
+  })
+})
 
 io.on('connection', function(socket) {
   console.log('a user connected');
 
-  // on([emitted key], function([emitted value]){} )
-  socket.on('chat-message', function(msg){
+  socket.on('chat-message', function(msg) {
     io.emit('chat-message-from-server', msg);
   });
   socket.on('disconnect', function() {
@@ -18,6 +66,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log("listening on 3000")
+server.start(function(){
+  console.log('shits on fire yo');
 })
